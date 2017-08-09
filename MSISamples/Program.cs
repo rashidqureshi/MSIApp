8e5f0802d4cd77ccbfb7c2c1f6d00bb20b872f61
rqureshi <rqureshi@microsoft.com>
2017-08-09 15:43:52 -0700

    Added token refresh

--- MSISamples/Program.cs
+++ MSISamples/Program.cs
@@ -16,12 +16,13 @@ namespace ConsoleApplication
 {
     public class Program
     {
+    
         public static void Main(string[] args)
         {
             var tenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
             var subscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
             var port = Environment.GetEnvironmentVariable("MSI_PORT");
-            if (new List<string> { tenantId, port, subscriptionId }.Any(i => String.IsNullOrEmpty(i)))
+           if (new List<string> { tenantId, port, subscriptionId }.Any(i => String.IsNullOrEmpty(i)))
            {
                 Console.WriteLine("Please provide ENV vars for AZURE_TENANT_ID, MSI_PORT and AZURE_SUBSCRIPTION_ID.");
            }
@@ -39,34 +40,81 @@ namespace ConsoleApplication
                Uri.EscapeDataString("https://management.azure.com/"),
                Uri.EscapeDataString(authority));
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(address);
-            StreamReader objReader = new StreamReader(request.GetResponse().GetResponseStream());
-            var jss = new JavaScriptSerializer();
-            var dict = jss.Deserialize<Dictionary<string, string>>(objReader.ReadLine());
+            Token token = getToken(address);
 
-            Write("Access token for ARM");
-            Write(dict["access_token"]);
-
+            Write("Access token for ARM -");
+            Write("Token = {0}",token.bearerToken);
+            Write("Expiry Time = {0}",token.expiryTime.ToString());
 
             // Intialize SDK using the token
-            var credentials = new TokenCredentials(dict["access_token"]);
+            var credentials = new TokenCredentials(token.bearerToken);
             var resourceClient = new ResourceManagementClient(credentials);
 
-            resourceClient.SubscriptionId = subscriptionId;
-
-            // List the resource group where VM has access to 
-            Write("Listing resource groups:");
-            resourceClient.ResourceGroups.List().ToList().ForEach(rg =>
+            // This while is to simulate a long running task that uses MSI token to access an Azure resource
+            while (true)
             {
-                Write("\tName: {0}, Id: {1}", rg.Name, rg.Id);
-            });
-            Write(Environment.NewLine);
+                // Re-try the operation if an exception is thrown due to expired token
+                while (true)
+                {
+                    try
+                    {
+                        resourceClient.SubscriptionId = subscriptionId;
+
+                        // List the resource group where VM has access to 
+                        Write("Listing resource groups:");
+                        resourceClient.ResourceGroups.List().ToList().ForEach(rg =>
+                        {
+                            Write("\tName: {0}, Id: {1}", rg.Name, rg.Id);
+                        });
+                        Write(Environment.NewLine);
+                        break;
+                    }
+                    catch (Microsoft.Rest.Azure.CloudException e)
+                    {
+                        // Get a new token
+                        if (DateTime.Now > token.expiryTime) 
+                        {
+                            token = getToken(address);
+                            credentials = new TokenCredentials(token.bearerToken);
+                            resourceClient = new ResourceManagementClient(credentials);
+                        } else
+                        {
+                            throw e;
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(10000);
+                Write(System.DateTime.Now.ToString());
+            }
+        }
+
+        private static Token getToken(string address)
+        {
+            Token token;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(address);
+            StreamReader objReader = new StreamReader(request.GetResponse().GetResponseStream());
             
+            var jss = new JavaScriptSerializer();
+            var dict = jss.Deserialize<Dictionary<string, string>>(objReader.ReadLine());
+            
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            Int64 expires_on = Convert.ToInt64(dict["expires_on"]);
+            epoch = epoch.AddSeconds(expires_on);
+            token.bearerToken = dict["access_token"];
+            token.expiryTime = epoch;
+
+            return token;
         }
 
         private static void Write(string format, params object[] items)
         {
             Console.WriteLine(String.Format(format, items));
+        }
+
+            private struct Token
+        {
+            public string bearerToken;
+            public DateTime expiryTime;
         }
     }
 }
